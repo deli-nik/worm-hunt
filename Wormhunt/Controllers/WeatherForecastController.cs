aa29ebc6ad818c11f8e41ae0afcb247b25299ac5
Nik Adli <nikadli@boostorder.com>
2022-06-03 14:14:09 +0800

    Convert React to use TypeScript.

--- Wormhunt/Controllers/WeatherForecastController.cs
+++ Wormhunt/Controllers/WeatherForecastController.cs
@@ -2,12 +2,8 @@
 
 namespace Wormhunt.Controllers
 {
-    /// <summary>
-    /// Testing controller documentation.
-    /// </summary>
     [ApiController]
     [Route("[controller]")]
-    [Produces("application/json")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -22,17 +18,13 @@ namespace Wormhunt.Controllers
             _logger = logger;
         }
 
-        /// <summary>
-        /// Get weather forecast.
-        /// </summary>
-        /// <returns>Weather forecast.</returns>
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 50),
+                TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
